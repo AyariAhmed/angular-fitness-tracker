898d5a9b67c63e5d69fb20988d13f323e64c5c13
AyariAhmed <ayari17@hotmail.fr>
2021-04-22 19:50:01 +0100

    Deployed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Angular Demo Project
+Deployed using firebase hosting [Visit demo website.](https://angular-rxjs-mui.web.app)
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,4 +1,4 @@
-import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import * as fromRoot from '../app.reducer';
@@ -9,7 +9,7 @@ import {take} from 'rxjs/operators';
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad{
 
-  constructor(private router:Router,private store : Store<fromRoot.State>) {
+  constructor(private store : Store<fromRoot.State>) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
