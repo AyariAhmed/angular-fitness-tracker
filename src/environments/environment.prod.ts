64662e0bb42f4b10f38a3ca55f1bfb32648ba0d6
AyariAhmed <ayari17@hotmail.fr>
2021-04-22 02:24:53 +0100

    added firebase to prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCkYyUU00lHIrZYoBbzwAKVQa8p-tePOZE",
+    authDomain: "angular-rxjs-mui.firebaseapp.com",
+    projectId: "angular-rxjs-mui",
+    storageBucket: "angular-rxjs-mui.appspot.com",
+    messagingSenderId: "282070541827",
+    appId: "1:282070541827:web:32a0fefaf02fbc2a8d9036",
+    measurementId: "G-63SSY646RV"
+  }
 };
